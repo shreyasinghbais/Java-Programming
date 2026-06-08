be87f9e70e0f38dfc9ac2e5dd3434701f7c34b3d
Shreya Singh Bais <124716473+shreyasinghbais@users.noreply.github.com>
2026-06-08 20:31:12 +0530

    Delete july5/Interface directory

--- july5/Interface/PaymentMethod.java
+++
@@ -1,6 +0,0 @@
-package july5.Interface;
-
-public interface PaymentMethod {
-	public void pay();
-	public void getPaymentDetails();
-}
--- july5/Interface/Printable.java
+++
@@ -1,5 +0,0 @@
-package july5.Interface;
-
-public interface Printable {
-	public void print();
-}
